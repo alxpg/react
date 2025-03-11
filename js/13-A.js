8284646723addc29c0468ec73f0cbe5f70e1c49d
alxpg <alxpega@gmail.com>
2025-03-10 23:19:08 -0600

    Mi ejercicio de tienda en 13-A.js

---
+++ js/13-A.js
@@ -0,0 +1,7 @@
+//Function Expression
+function tienda(abarrotes, limpieza, farmacia){
+    console.log(abarrotes + limpieza + farmacia);
+}
+
+tienda ('Leche', 'Pan', 'Huevos', 'Frijoles', 'Arroz', 'Azucar', 'Sal', 'Aceite', 'Cafe' );
+
