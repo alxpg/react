b1945a733b788b9f5b922ea2164f60121443646d
alxpg <alxpega@gmail.com>
2025-03-14 12:46:46 -0600

    ejercicio 17.js

--- js/17.js
+++ js/17.js
@@ -20,7 +20,7 @@ const nuevo2_Array = numbers.filter(number => console.log(number)) // Se puede s
 //Ahora vamos a sacar solo un elemento del arreglo
 
 const nuevo3Array = tecnologias.filter(tech => tech === 'react') // De esta manera solo se va a mostrar el elemento que sea igual a 'react'
-const nuevo3_Array = numbers.filter(number => number === 10)
+const nuevo3_Array = numbers.filter(number => number === 10) // Este metodo tambien es llamado Callback function ya que se esta pasando una funcion como argumento
 console.table(nuevo3Array)
 console.table(nuevo3_Array)
 
@@ -30,10 +30,14 @@ const nuevo4_Array = numbers.filter(number => number !== 10)
 console.table(nuevo4Array)
 console.table(nuevo4_Array)
 
-//Metod de array includes.
+//Metodo de array includes.
 //Este metodo nos va a permitir saber si un elemento esta dentro de un arreglo o no.
 
 const resultado = tecnologias.includes('react') // Si el elemento esta dentro del arreglo nos va a devolver un true
 const resultado2 = tecnologias.includes('angular') // Si el elemento no esta dentro del arreglo nos va a devolver un false
 console.log(resultado)
 console.log(resultado2)
+
+// Some - Devuelve true si al menos un elemento cumple con la condición
+// Every - Devuelve true si todos los elementos cumplen con la condición 
+
