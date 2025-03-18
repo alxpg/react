337c746eb125ff8f89c8d0f806af2204867f095f
alxpg <alxpega@gmail.com>
2025-03-18 14:08:59 -0600

    Arreglos 17.js

--- js/17.js
+++ js/17.js
@@ -35,9 +35,28 @@ console.table(nuevo4_Array)
 
 const resultado = tecnologias.includes('react') // Si el elemento esta dentro del arreglo nos va a devolver un true
 const resultado2 = tecnologias.includes('angular') // Si el elemento no esta dentro del arreglo nos va a devolver un false
+
 console.log(resultado)
 console.log(resultado2)
 
-// Some - Devuelve true si al menos un elemento cumple con la condición
-// Every - Devuelve true si todos los elementos cumplen con la condición 
+const resultado3 = numbers.some(numbers => numbers > 8 )// Some - Devuelve true si al menos un elemento cumple con la condición
+if (resultado3) {
+    console.log('Si hay numeros mayores a: ' + 8)
+} else {
+    console.log('No hay numeros mayores a: ' + 8)
+}
 
+const resultado4 = numbers.find(numbers => numbers > 10)//FIND - Devuelve el primer elemento que cumpla con la condición
+console.log(resultado4)
+
+
+const resultado5 = numbers.every(numbers => numbers >10)// Every - Devuelve true si todos los elementos cumplen con la condición 
+console.log(resultado5)
+
+const resultado6 = numbers.reduce((total, numero) => { //reduce - Reduce todos los elementos de un arreglo a un solo valor
+    console.log(total)
+   // console.log(numero)
+    return total + numero // En este caso se va a sumar todos los elementos del arreglo
+}, 0) // El 0 es el valor inicial de la variable total
+
+// console.log(resultado6)
