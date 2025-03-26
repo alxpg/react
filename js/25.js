6d7f8d7ea4cab715f2d9003db5c7206c7d37c133
alxpg <alxpega@gmail.com>
2025-03-26 12:26:34 -0600

    FETCH API y Promises

---
+++ js/25.js
@@ -0,0 +1,23 @@
+//Fetch API va muy de la mano con PROMISES.
+/**Fetch API proporciona una interfaz para integrar recursos para poder obtener datos a traves de la red */
+
+/**PROMISES es un objeto que va a estar disponible a futuro o no
+ * Los PROMISES cuentan con varios pasos y con varios estados
+ * Los pasos se representan por una sintaxis .then()
+*/
+
+const url = 'https://jsonplaceholder.typicode.com/comments';
+
+fetch(url) //Quiero obtener los datos de esta URL
+    .then((response)=>{ //Entonces que quiero hacer
+        if(response.ok){
+        return response.json()
+        }   
+        throw new Error ('Error al obtener los datos')
+    }) 
+    .then(data =>{ 
+        console.log(data) 
+    })
+    .catch(error => {  // Solo se ejecuta cuando hay problemas de red
+        console.log(error) 
+    })
